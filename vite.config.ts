f1abdc225cc0b06b3015cb7bb6d226b55f65ba08
DreamURL <oodework@gmail.com>
2026-02-14 16:41:34 +0900

    Update base URL to /reaction-time-test/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
+  base: '/reaction-time-test/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
